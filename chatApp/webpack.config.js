fcf8c886c20c8a1194161f88b4575d4baf319e8c
potsrevennil <b02901144@ntu.edu.tw>
2015-11-16 10:06:03 +0800

    modify path of bundle

--- chatApp/webpack.config.js
+++ chatApp/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: 'dist/'
+    publicPath: '/webProgramming/chatApp/dist/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
