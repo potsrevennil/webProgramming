948b4d6c0ad98a73b5850f27fca77e37f2e0f958
potsrevennil <b02901144@ntu.edu.tw>
2015-11-19 17:28:58 +0800

    start cardGame program

---
+++ cardGame/src/cardGame.js
@@ -0,0 +1,7 @@
+const React = require('react') ;
+const Rebase = require('re-base');
+const base = Rebase.createClass('https://chatappoflin.firebaseio.com');
+
+
+
+module.exports= cardGame ;
---
+++ cardGame/src/index.js
@@ -0,0 +1,7 @@
+const React = require('react') ;
+const {render} = require('react-dom') ;
+const cardGame = require('./cardGame') ;
+
+
+
+render(<cardGame />, document.getElementById('root')) ;
