2c32eae35dfc67ca933ecf5240ce592ac2e5ddaf
potsrevennil <b02901144@ntu.edu.tw>
2015-11-23 20:24:30 +0800

    mainDeck flip card successgit add ../chatApp/src/style.css

--- cardGame/src/CardGame.js
+++ cardGame/src/CardGame.js
@@ -3,43 +3,43 @@ const React = require('react') ;
 // const base = Rebase.createClass('https://chatappoflin.firebaseio.com');
 //const imgPath = 'images/1';
 const cardDeck= [];
-const cardColors = ['spade', 'diamond', 'heart', 'club'];
+const cardColors = ['spade', 'heart', 'diamond', 'club'];
 for (let i = 1; i <= 13; i++) {
-    cardColors.forEach((color) => {
-        cardDeck.push({
-            cardValue: `${i}`,
-            cardColor: color,
-            cardFront: `images/1/${color[0]}${i}.gif`,
-            cardBack: 'images/1/b2fv.gif',
-        });
+  cardColors.forEach((color) => {
+    cardDeck.push({
+      cardValue: `${i}`,
+      cardColor: color,
+      cardFront: `images/1/${color[0]}${i}.gif`,
+      cardBack: 'images/1/b2fv.gif',
     });
+  });
 }
 
 const initialState = {
     mainDeck: cardDeck,
-    dealDeck: {},
+    dealDeck: [],
     playingDecks: [
       {
         playingDeck:[
           {
-            cardValue: "1",
-            cardColor: "spade",
-            cardFront: "images/1/s1.gif",
-            cardBack: "images/1/b2fv.gif"
+            cardValue: '1',
+            cardColor: 'spade',
+            cardFront: 'images/1/s1.gif',
+            cardBack: 'images/1/b2fv.gif'
           }
         ]
       },{
         playingDeck:[
           {
-            cardValue: "2",
-            cardColor: "spade",
-            cardFront: "images/1/s2.gif",
-            cardBack: "images/1/b2fv.gif"
+            cardValue: '2',
+            cardColor: 'spade',
+            cardFront: 'images/1/s2.gif',
+            cardBack: 'images/1/b2fv.gif'
           },{
-            cardValue: "3",
-            cardColor: "spade",
-            cardFront: "images/1/s3.gif",
-            cardBack: "images/1/b2fv.gif"
+            cardValue: '3',
+            cardColor: 'spade',
+            cardFront: 'images/1/s3.gif',
+            cardBack: 'images/1/b2fv.gif'
           }
         ]
       }
@@ -51,7 +51,16 @@ class CardGame extends React.Component {
     super(props) ;
     this.state = initialState ;
   }
-  
+  flipCard(mainDeckTop){
+    var{mainDeck, dealDeck}= this.state;
+    //const mainDeckTop= mainDeck[mainDeck.length-1]; 
+    mainDeck.pop();
+    dealDeck.push(mainDeckTop);
+    this.setState({
+        mainDeck: mainDeck,
+        dealDeck: dealDeck
+    });
+  }
   renderMainDeckCard(card, i){
     const{ cardValue, cardColor, cardFront, cardBack}= card;
     return(
@@ -76,19 +85,28 @@ class CardGame extends React.Component {
 
   render(){
     const { mainDeck, dealDeck, playingDecks } = this.state ; 
+    const mainDeckTop= mainDeck[mainDeck.length- 1];
+    const dealDeckTop= dealDeck[dealDeck.length- 1] || {};
     return(
-      <div className= "cardGame">
-        <div className= "toolBar">
-          <h3 className = "gameButton"> Game </h3>
+      <div className= 'cardGame'>
+        <div className= 'toolBar'>
+          <h3 className = 'gameButton'> Game </h3>
         </div>
-        <div className= "field">
-          <div className= "mainDeck">
-            {mainDeck.map(this.renderMainDeckCard,this)}
+        <div className= 'field'>
+          <div className= 'mainDeck'>
+            <img className= 'card' 
+              src= {mainDeckTop.cardBack}
+              onClick= {this.flipCard.bind(this,mainDeckTop)}
+              className= 'img' 
+            />
           </div>
-          <div className= "dealDeck">
-            <img className= "card" src= {dealDeck.cardFront} alt= "" className= "img" />
+          <div className= 'dealDeck'>
+            <img className= 'card'
+            src= {dealDeckTop.cardFront} 
+            className= 'img' 
+            />
           </div>
-          <div className= "playingDecks">
+          <div className= 'playingDecks'>
             {playingDecks.map(this.renderPlayingDeck, this)}
           </div>
         </div>
@@ -110,7 +128,7 @@ class MainDeckCard extends React.Component{
     const {index, value, color, cardBack} = this.props ;
     return(
       <div>
-        <img className= "mainDeckCard" src= {cardBack} alt= "" className= "img" />
+        <img className= 'mainDeckCard' src= {cardBack} alt= '' className= 'img' />
       </div>
     );
   }
@@ -133,7 +151,7 @@ class PlayingDeck extends React.Component{
   render(){
     const {playingDeck} = this.props ;
     return(
-      <div className= "cards">
+      <div className= 'cards'>
         {playingDeck.map(this.renderPlayingDeckCard,this)}
       </div>
     );
@@ -145,7 +163,7 @@ class PlayingDeckCard extends React.Component{
     const {index, value, color, cardFront, cardBack} = this.props ;
     return(
       <div>
-        <img className= "playingDeckCard" src= {index== 0? cardFront: cardBack}  alt= "" className= "img" />
+        <img className= 'playingDeckCard' src= {index== 0? cardFront: cardBack}  alt= '' className= 'img' />
       </div>
     );
   }
